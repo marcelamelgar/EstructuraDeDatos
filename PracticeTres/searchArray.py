a76ff4f716477824518b8906c83bc0a2a7b6cd38
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2021-02-09 18:14:21 -0600

    compostura random

--- PracticeTres/searchArray.py
+++ PracticeTres/searchArray.py
@@ -1,18 +1,17 @@
 from flask import Flask
-from numpy import random
+from random import randint
 
 app = Flask(__name__)
 
 def randomArr(long):
     randomArray= []
     for i in range(0, long):
-        x = random.randint(100, size = (long))
-        randomArray.append(x)
+        randomArray.append((randint(0,100)))
     return randomArray
 
 def linear(array, num):
     for i in range(len(array)):
-        if array[i] == num:
+        if (array[i] == num):
             return True
     return False
 
@@ -24,23 +23,22 @@ def binary(arr, l, r, x):
             return True
         elif arr[mid] < x: 
             l = mid + 1                 #sacado de geeksforgeeks
-  
         else: 
             r = mid - 1
     return False
 
-@app.route('/linear/<int:length>/<int:value>/')
-def linearSearch(length, value):
-    elementos = randomArr(length)
-    if linear(elementos, value) == True:
+@app.route('/linear/<int:array>/<int:num>/')
+def linearSearch(array, num):
+    elementos = randomArr(array)
+    if (linear(elementos, num)) == True:
         return 'se ha hallado el valor en el arreglo\n'
     else:
         return 'no se ha hallado el valor en el arreglo\n'
 
-@app.route('/binary/<int:length>/<int:value>/')
-def binarySearch(length, value):
-    elementos = randomArr(length)
-    if binary(elementos, 0, len(elementos)-1, value) == True:
+@app.route('/binary/<int:array>/<int:num>/')
+def binarySearch(array, num):
+    elementos = randomArr(array)
+    if (binary(elementos, 0, len(elementos)-1, num)) == True:
         return 'se ha hallado el valor en el arreglo\n'
     else:
         return 'no se ha hallado el valor en el arreglo\n'
